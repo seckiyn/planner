8662be2fc2e85675cb3fffa654a3da9353780564
Mustafa Akkaya <kyofl6@gmail.com>
2022-03-11 18:57:10 +0300

    Added argument parser

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 """ Main script """
+import sys
+import argparse
 import lit
 from inputs import handle_input as hinp
 
 
+# TODO: Argument parser
 def main():
     """ Main loop function """
     running = True
@@ -20,6 +23,18 @@ def main():
                 print("There's no function like that", my_func)
         else:
             lit.run_function(answer)
+def args():
+    """ Parse the arguments if there's any """
+    # TODO: Add a way to record without entering program
+    ap = argparse.ArgumentParser() # Argument parser
+    ap.add_argument("-f", "--function", required=True,\
+            help="Function to make") # Function argument
+    args = vars(ap.parse_args()) # Parse the arguments
+    function = args["function"]
+    lit.run_function(function)
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) > 1:
+        args()
+    else:
+        main()
